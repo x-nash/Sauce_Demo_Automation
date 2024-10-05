1fa451e27ef9fb683ba0f77c7f28bc0f979b5da2
x-nash <nashita.shameena@gmail.com>
2024-10-05 12:10:48 +0530

    Integrated allure reporting (with error)

--- wdio.conf.js
+++ wdio.conf.js
@@ -135,7 +135,12 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: [ 'spec'],
+    reporters: [ 'spec', ['allure', {
+      outputDir: 'allure-results',
+      disableWebdriverStepsReporting: true,
+      disableWebdriverScreenshotsReporting: false,
+      }], 
+    ],
 
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
@@ -266,6 +271,13 @@ export const config = {
      */
     // afterStep: function (step, scenario, result, context) {
     // },
+    afterStep: async function (step, scenario, { error, duration, passed }, context) {
+      if (error) {
+        //await browser.takeScreenshot();
+        var date = Date.now();
+        browser.saveScreenshot("./allure-results/screenshots/Chrome-" + date + ".png")
+      }
+    },
     /**
      *
      * Runs after a Cucumber Scenario.
@@ -323,6 +335,26 @@ export const config = {
      */
     // onComplete: function(exitCode, config, capabilities, results) {
     // },
+    onComplete: function() {
+      const reportError = new Error('Could not generate Allure report')
+      const generation = allure(['generate', 'allure-results', '--clean'])
+      return new Promise((resolve, reject) => {
+          const generationTimeout = setTimeout(
+              () => reject(reportError),
+              5000)
+
+          generation.on('exit', function(exitCode) {
+              clearTimeout(generationTimeout)
+
+              if (exitCode !== 0) {
+                  return reject(reportError)
+              }
+
+              console.log('Allure report successfully generated')
+              resolve()
+          })
+      })
+  },
     /**
     * Gets executed when a refresh happens.
     * @param {string} oldSessionId session ID of the old session
